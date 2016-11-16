44687c434f2000226a059b5b1f073f0d101f5e04
Valens Iraguha <valens.iraguha@yahoo.com>
2016-11-16 08:53:12 +0100

    Update TouchSensor.java

--- TouchSensor.java
+++ TouchSensor.java
@@ -1,1 +1,30 @@
+/**
+ * @author : IRAGUHA VALENS 
+ **/
 
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+
+public class TouchSensor {	
+
+	private static EV3TouchSensor BoutonTemps;
+	private static SimpleTouch BoutonTS;
+	
+	public static void main(String	[] args) throws InterruptedException { 
+		BoutonTemps = new EV3TouchSensor(SensorPort.S1); 
+		BoutonTS = new SimpleTouch(BoutonTemps); 
+		LCD.clear();
+			
+		while (!Button.ESCAPE.isDown()) { 
+			boolean a = BoutonTS.isPressed(); 
+		    LCD.drawString("TS: " + a, 0, 0); 
+			Thread.sleep(100); 
+		}
+
+		LCD.clear(); 
+		Sound.beepSequence(); 
+	}
+}
