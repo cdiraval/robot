88eb71303183f553866c611a56a841cac7cacbe7
Valens Iraguha <valens.iraguha@yahoo.com>
2016-11-16 08:51:46 +0100

    Update SimpleTouch.java

--- SimpleTouch.java
+++ SimpleTouch.java
@@ -1,1 +1,25 @@
+/**
+ * @author : IRAGUHA VALENS 
+ **/
 
+import lejos.robotics.SampleProvider;
+import lejos.robotics.filter.AbstractFilter;
+
+public class SimpleTouch extends AbstractFilter {
+  
+    private float[] sample;
+
+  public SimpleTouch(SampleProvider source) {
+    super(source); 
+    sample = new float[sampleSize]; 
+  }
+
+  public boolean isPressed() {
+    super.fetchSample(sample, 0); 
+    if (sample[0] == 0) {  
+      return false;
+    }
+    else return true;
+  }
+
+}
